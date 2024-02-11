c1ff959ac82bba7069e11b334a08bbfda3d7f8d6
RishadHarahap <rishadaufaharahap@gmail.com>
2024-02-11 20:37:07 +0700

    Successfully Add Style To Registration Page

--- db/migrate/20240211101436_devise_create_users.rb
+++ db/migrate/20240211101436_devise_create_users.rb
@@ -6,6 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name, null: false
+      t.integer :age, null: false
+      t.string :education, null: false
+      t.string :job, null: false
 
       ## Recoverable
       t.string   :reset_password_token
