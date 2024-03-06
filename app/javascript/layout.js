ccaee24f6e89a471107a412828e927ebbcb011f2
RishadHarahap <rishadaufaharahap@gmail.com>
2024-03-06 23:06:26 +0700

    Make The Loader Fixed And Add Locomotive Scrolling

--- app/javascript/layout.js
+++ app/javascript/layout.js
@@ -2,6 +2,14 @@
 //         $(".loadercontainer").fadeOut("slow");
 //     });
 
+// $(window).on('beforeunload', function(){
+//       $(window).scrollTop(0);
+// });
+
     $(window).ready(function() {
         $(".loadercontainer").fadeOut("slow")
     })
+
+    $(function () {
+        const locomotiveScroll = new LocomotiveScroll();
+    })();
