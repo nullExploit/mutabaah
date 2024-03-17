ec011d1f45087248b1ddd582c928b9ab27958108
RishadHarahap <rishadaufaharahap@gmail.com>
2024-03-17 15:06:14 +0700

    Add Validation Sign Up (Late)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :murojaahs, dependent: :destroy
+  validates :name, :age, presence: true
 end
